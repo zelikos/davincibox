dbe4d83f255759e652a53517d1023c124bbb5ee4
zelikos <pcsikos@zelikos.dev>
2023-09-06 14:27:54 -0400

    feat: Add distrobox commands

--- setup.sh
+++ setup.sh
@@ -1,18 +1,64 @@
 #!/bin/bash
 
-# TODOs
+use_distrobox=false
 
 # Check that argument containing DaVinci_Resolve_version_LInux.run was provided
-# Check if distrobox is installed
-# If no distrobox, check for toolbox
-# If neither are installed, inform the user that one of them is needed and exit
+if [[ $1 ]]
+then
+    # Check if distrobox is installed
+    if ! command -v distrobox &> /dev/null
+    then
+        # If no distrobox, check for toolbox
+        echo "Distrobox not found. Checking for toolbox..."
+        if ! command -v toolbox &> /dev/null
+        then
+            echo "Toolbox not found."
+            # If neither are installed, inform the user, then exit
+            echo "Please install either distrobox or toolbox to use this script."
+            exit
+        else
+            echo "Toolbox found."
+            # TODO: Add Toolbox support
+            echo "Toolbox support is currently WIP. Please use distrobox for now."
+            exit
+        fi
+    else
+        use_distrobox=true
+        echo "Distrobox found."
+    fi
+else
+    echo "Please provide path to DaVinci Resolve installer file."
+    echo "e.g. ./setup.sh /path/to/folder/DaVinci_Resolve_18.1.4_linux.run"
+    exit
+fi
 
 # Create davincibox on user's system
+echo "Setting up davincibox..."
 
-# Run DaVinci_Resolve_version_Linux.run --appimage-extract
-# Run sudo squashfs-root/AppRun -i -a -y from davincibox
+if $use_distrobox
+then
+    distrobox create -i ghcr.io/zelikos/davincibox:latest -n davincibox
+else
+    toolbox create -i ghcr.io/zelikos/davincibox:latest -c davincibox
+fi
 
-# Run sudo rm /opt/resolve/libs/libglib-2.0.* from davincibox
+# Extract DaVinci Resolve installer
+$1 --appimage-extract
+if [ $? -eq 0 ]
+then
+    if $use_distrobox
+    then
+        distrobox enter davincibox -- sudo squashfs-root/AppRun -i -a -y
+        # Workaround for an issue with Resolve's included libglib-2.0
+        # May not be needed in the future
+        distrobox enter davincibox -- sudo rm /opt/resolve/libs/libglib-2.0.*
+    # TODO: Add toolbox support
+    fi
+else
+    echo "${1} is not a DaVinci Resolve installer."
+    exit
+fi
 
+# Prompt user about adding desktop launcher
 # If using distrobox, run distrobox-export --app /opt/resolve/bin/resolve from davincibox
 # Else, do toolbox steps

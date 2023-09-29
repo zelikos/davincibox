27f371ee734add0cf9a94a386e05573c97489e1a
Patrick Csikos <pcsikos@zelikos.dev>
2023-09-29 13:04:21 -0400

    feat: Add "remove" option in setup.sh (#15)
    * refactor: Don't check argument validity in setup.sh
    
    The check is already done in setup-davinci, so it is unneeded here.
    
    * feat: Add remove option
    
    * docs: Mention 'setup.sh remove' for uninstallation

--- setup.sh
+++ setup.sh
@@ -2,52 +2,63 @@
 
 container_type=""
 
-# Check that argument containing DaVinci_Resolve_version_LInux.run was provided
-if [[ -f $(readlink -e $1) ]]
+# Check if distrobox is installed
+if ! command -v distrobox &> /dev/null
 then
-    # Check if distrobox is installed
-    if ! command -v distrobox &> /dev/null
+    # If no distrobox, check for toolbox
+    echo "Distrobox not found. Checking for toolbox..."
+    if ! command -v toolbox &> /dev/null
     then
-        # If no distrobox, check for toolbox
-        echo "Distrobox not found. Checking for toolbox..."
-        if ! command -v toolbox &> /dev/null
-        then
-            echo "Toolbox not found."
-            # If neither are installed, inform the user, then exit
-            echo "Please install either distrobox or toolbox to use this script."
-            exit
-        else
-            container_type="toolbox"
-            echo "Toolbox found."
-        fi
+        echo "Toolbox not found."
+        # If neither are installed, inform the user, then exit
+        echo "Please install either distrobox or toolbox to use this script."
+        exit
     else
-        container_type="distrobox"
-        echo "Distrobox found."
+        container_type="toolbox"
+        echo "Toolbox found."
     fi
 else
-    echo "Please provide path to DaVinci Resolve installer file."
-    echo "e.g. ./setup.sh /path/to/folder/DaVinci_Resolve_18.1.4_linux.run"
-    exit
+    container_type="distrobox"
+    echo "Distrobox found."
 fi
 
-# Create davincibox on user's system
-echo "Setting up davincibox..."
 
-if [[ $container_type == "distrobox" ]]
+if [[ $1 == "remove" ]]
 then
-    distrobox create -i ghcr.io/zelikos/davincibox:latest -n davincibox
-    # Start up the container now after creation,
-    # rather than during the later steps
-    distrobox enter davincibox -- echo "davincibox initialized"
+    echo "Removing DaVinci Resolve and davincibox..."
+    if [[ $container_type == "distrobox" ]]
+    then
+        distrobox enter davincibox -- add-davinci-launcher remove
+        distrobox stop davincibox
+        distrobox rm davincibox
+    elif [[ $container_type == "toolbox" ]]
+    then
+        toolbox run --container davincibox add-davinci-launcher remove
+        podman container stop davincibox
+        toolbox rm davincibox
+    fi
+    echo "davincibox removed."
 else
-    toolbox create -i ghcr.io/zelikos/davincibox:latest -c davincibox
-    toolbox run --container davincibox echo "davincibox initialized"
-fi
+    # Create davincibox on user's system
+    echo "Setting up davincibox..."
 
-# Run setup-davinci
-if [[ $container_type == "distrobox" ]]
-then
-    distrobox enter davincibox -- setup-davinci $1 $container_type
-else
-    toolbox run --container davincibox setup-davinci $1 $container_type
+    if [[ $container_type == "distrobox" ]]
+    then
+        distrobox create -i ghcr.io/zelikos/davincibox:latest -n davincibox
+        # Start up the container now after creation,
+        # rather than during the later steps
+        distrobox enter davincibox -- echo "davincibox initialized"
+    else
+        toolbox create -i ghcr.io/zelikos/davincibox:latest -c davincibox
+        toolbox run --container davincibox echo "davincibox initialized"
+    fi
+
+    # Run setup-davinci
+    if [[ $container_type == "distrobox" ]]
+    then
+        distrobox enter davincibox -- setup-davinci $1 $container_type
+    else
+        toolbox run --container davincibox setup-davinci $1 $container_type
+    fi
+
 fi

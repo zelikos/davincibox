34020091491afe0bfeabc9486a96ac46e69bf22b
zelikos <pcsikos@zelikos.dev>
2023-09-29 12:48:23 -0400

    refactor: Don't check argument validity in setup.sh
    The check is already done in setup-davinci, so it is unneeded here.

--- setup.sh
+++ setup.sh
@@ -2,32 +2,24 @@
 
 container_type=""
 
-# Check that argument containing DaVinci_Resolve_version_LInux.run was provided
-if [[ -f $(readlink -e $1) ]]
+# Check if distrobox is installed
+if ! command -v distrobox &> /dev/null
 then
-    # Check if distrobox is installed
-    if ! command -v distrobox &> /dev/null
+    # If no distrobox, check for toolbox
+    echo "Distrobox not found. Checking for toolbox..."
+    if ! command -v toolbox &> /dev/null
     then
-        # If no distrobox, check for toolbox
-        echo "Distrobox not found. Checking for toolbox..."
-        if ! command -v toolbox &> /dev/null
-        then
-            echo "Toolbox not found."
-            # If neither are installed, inform the user, then exit
-            echo "Please install either distrobox or toolbox to use this script."
-            exit
-        else
-            container_type="toolbox"
-            echo "Toolbox found."
-        fi
+        echo "Toolbox not found."
+        # If neither are installed, inform the user, then exit
+        echo "Please install either distrobox or toolbox to use this script."
+        exit
     else
-        container_type="distrobox"
-        echo "Distrobox found."
+        container_type="toolbox"
+        echo "Toolbox found."
     fi
 else
-    echo "Please provide path to DaVinci Resolve installer file."
-    echo "e.g. ./setup.sh /path/to/folder/DaVinci_Resolve_18.1.4_linux.run"
-    exit
+    container_type="distrobox"
+    echo "Distrobox found."
 fi
 
 # Create davincibox on user's system

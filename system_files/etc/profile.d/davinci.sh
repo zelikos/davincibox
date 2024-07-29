2ea40eb8de2cd29d0476f09cabb5e507b79034c9
Zeglius <33781398+Zeglius@users.noreply.github.com>
2024-07-29 16:49:41 +0200

    refactor: Utilize patchelf instead of LD_PRELOAD (#109)
    This will remove the need of using LD_PRELOAD as of
    https://www.reddit.com/r/voidlinux/comments/12g71x0/comment/l2cwo27/ by
    forcing DaVinci Resolve binaries to use the listed libraries at the
    specified absolute paths:
    - /usr/lib64/libglib-2.0.so.0
    - /usr/lib64/libgdk_pixbuf-2.0.so.0
    - /usr/lib64/libgio-2.0.so.0
    - /usr/lib64/libgmodule-2.0.so.0

--- system_files/etc/profile.d/davinci.sh
+++ system_files/etc/profile.d/davinci.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Info for this line comes from here: https://www.reddit.com/r/voidlinux/comments/12g71x0/comment/l2cwo27/
-# Slightly modified, as the original wasn't pointed to the correct lib file names
-export LD_PRELOAD=/usr/lib64/libglib-2.0.so.0:/usr/lib64/libgdk_pixbuf-2.0.so.0:/usr/lib64/libgio-2.0.so.0:/usr/lib64/libgmodule-2.0.so.0
 export QT_QPA_PLATFORM=xcb
 
 gpu_type=""

07eb32eaf1e95c118db721a625f8e1de197c09d4
zelikos <pcsikos@zelikos.dev>
2024-08-15 04:48:04 -0400

    docs(davinci.sh): Explain Nvidia and AMD GPU checks

--- system_files/etc/profile.d/davinci.sh
+++ system_files/etc/profile.d/davinci.sh
@@ -5,8 +5,11 @@ export QT_QPA_PLATFORM=xcb
 gpu_type=""
 
 get_gpu_type () {
+  # Checks only for nvidia driver and not nouveau, as nouveau is handled differently than the proprietary driver
   if lshw -c video 2>/dev/null | grep -qi "driver=nvidia"; then
     gpu_type="nvidia"
+  # Checks for amdgpu and not radeon, as any GPUs that outright require the radeon driver
+  # are *very* unlikely to handle running DaVinci Resolve anyway
   elif lshw -c video 2>/dev/null | grep -qi "driver=amdgpu"; then
     gpu_type="amd"
   # TODO: Check for Intel driver name(s) instead
